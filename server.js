983f8981e2ea1dfa520f5454ce00858a7f117103
prafullsharma98 <prafullsharma48@gmail.com>
2017-09-16 16:35:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 app.get('/article-one',function(req,res){
     
-    res.send('article one is rsponding');
+    res.send('article one is rsponding very well');
 });
 app.get('/article-two',function(req,res){
     
