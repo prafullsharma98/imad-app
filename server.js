df9d8a8e04acea472ccb131cd085cafc931b263b
prafullsharma98 <prafullsharma48@gmail.com>
2017-09-16 16:08:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-   res.send('article ine is requested'); 
-    
-});
+
 app.get('/Article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui' , 'Article-one.html '));
 });
