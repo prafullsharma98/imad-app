9f061dc7e8c6d3bcc1672a2dacdde275847055b0
prafullsharma98 <prafullsharma48@gmail.com>
2017-09-21 23:22:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,11 +82,12 @@ var htmlTemplate =
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var counter =0;
+var counter = 0;
 app.get('/counter',function(req,res){
-   counter = counter+1;
-   res.send(counter.toString());
+    counter = counter+1;
+    res.send(counter);    
 });
+
 app.get('/ui/style.css',function(req,res){
     res.sendFile(path.join(__dirname,'ui','style.css'));
 });
