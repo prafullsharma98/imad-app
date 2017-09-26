e6eb0502cda56d5e4782522b4d455f73050415e7
prafullsharma98 <prafullsharma48@gmail.com>
2017-09-27 00:24:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
   
 var counter = 0;
 app.get('/counter',function(req,res){
-   counter = counter+2;
+   counter = counter+1;
    res.send(counter.toString());
 });
 app.get('/ui/style.css',function(req,res){
