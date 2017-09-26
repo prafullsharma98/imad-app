d8e4ee8ff4222b75adb452d59eb49feea502209b
prafullsharma98 <prafullsharma48@gmail.com>
2017-09-27 01:06:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 var a = document.getElementById('txt2');
 var counter = 0;
-a.onclick = function(){
+onclick.a= function(){
     
     counter = counter+1;
     var b = document.getElementById('count');
