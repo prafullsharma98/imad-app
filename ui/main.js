dd82f030a23c5eec85b0f466fa0911cb1d0911eb
prafullsharma98 <prafullsharma48@gmail.com>
2017-09-27 20:07:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,15 +5,15 @@ a.onclick= function(){
    var x = new XMLHttpRequest();
    x.onreadystatechange = function()
    {
-     if(x.readyState === XMLHttpRequest.DONE)
-     {
+     
+     
          if(x.status === 304)
          {
              var txt2 = x.responseText;
              var span = document.getElementById('count');
              span.innerHTML = txt2.toString();
          }
-     }
+     
     
    };
      x.open('GET','http://prafullsharma48.imad.hasura-app.io/counter',true);
